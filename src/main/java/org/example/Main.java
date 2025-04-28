6f7fc2a35314e637c9b4cf759c88eb3f1c332a28
diaskorpesh <118056718+diaskorpesh@users.noreply.github.com>
2025-04-28 09:25:09 +0500

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,23 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        MyHashTable<MyTestingClass, Student> h = new MyHashTable<>();
-        for (int i = 0; i < 10000; i++)
-            h.put(new MyTestingClass(i), new Student(i, "s" + i));
-        int[] b = h.bucketSizes();
-        for (int i = 0; i < b.length; i++)
-            System.out.println(i + " " + b[i]);
-
-        BST<Integer, String> t = new BST<>();
-        for (int i = 0; i < 20; i++) t.put(i, "v" + i);
-        for (BST.Entry<Integer, String> e : t)
-            System.out.println("key is " + e.getKey() + " and value is " + e.getValue());
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+        for (int i = 0; i < 10000; i++) {
+            int randomId = (int) (Math.random() * 1000000);
+            table.put(new MyTestingClass(randomId), new Student("Student" + i));
+        }
+        int[] bucketSizes = table.bucketSizes();
+        System.out.println("Hash Table Bucket Sizes:");
+        for (int i = 0; i < bucketSizes.length; i++) {
+            System.out.println("Bucket " + i + ": " + bucketSizes[i]);
+        }
+        BinarySearchTree<Integer, String> tree = new BinarySearchTree<>();
+        for (int i = 0; i < 20; i++) {
+            tree.put(i, "v" + i);
+        }
+        System.out.println("\nTree size: " + tree.size());
+        for (BinarySearchTree.Entry<Integer, String> elem : tree) {
+            System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
+        }
     }
 }
